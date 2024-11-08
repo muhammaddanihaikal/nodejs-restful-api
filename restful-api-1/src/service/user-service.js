9fa2cb67abc536bf735975af066c40dd6808510d
muhammaddanihaikal <dainnaxjakarta91@gmail.com>
2024-11-09 06:24:03 +0700

    menambahkan login validation, serive, dan controller

--- restful-api-1/src/service/user-service.js
+++ restful-api-1/src/service/user-service.js
@@ -1,8 +1,13 @@
 import { prismaClient } from "../application/database.js";
-import { registerUserValidation } from "../validation/user-validation.js";
+import {
+  loginUserValidation,
+  registerUserValidation,
+} from "../validation/user-validation.js";
 import { validate } from "../validation/validation.js";
 import { ResponseError } from "../error/response-error.js";
 import bcrypt from "bcrypt";
+import { logger } from "../application/logging.js";
+import { v4 as uuid } from "uuid";
 
 const register = async (request) => {
   // validasi
@@ -33,4 +38,45 @@ const register = async (request) => {
   });
 };
 
-export default { register };
+const login = async (request) => {
+  const loginRequest = validate(loginUserValidation, request);
+
+  const user = await prismaClient.user.findUnique({
+    where: {
+      username: loginRequest.username,
+    },
+    select: {
+      username: true,
+      password: true,
+    },
+  });
+
+  if (!user) {
+    throw new ResponseError(401, "Username or password wrong");
+  }
+
+  const isPasswordValid = await bcrypt.compare(
+    loginRequest.password,
+    user.password
+  );
+
+  if (!isPasswordValid) {
+    throw new ResponseError(401, "Username or password wrong");
+  }
+
+  const token = uuid.toString();
+
+  return await prismaClient.user.update({
+    where: {
+      username: user.username,
+    },
+    data: {
+      token,
+    },
+    select: {
+      token: true,
+    },
+  });
+};
+
+export default { register, login };
--- restful-api-1/src/validation/validation.js
+++ restful-api-1/src/validation/validation.js
@@ -3,6 +3,7 @@ import { ResponseError } from "../error/response-error.js";
 const validate = (schema, request) => {
   const result = schema.validate(request, {
     abortEarly: false,
+    allowUnknown: false, // ketika user menambahkan field sembarang tidak bisa (default: false)
   });
 
   if (result.error) {
